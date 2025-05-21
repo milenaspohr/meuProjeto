fd10872bf4a1363a76eb7532f8830507ba946b72
milenaspohr <milena.spohr@aluno.feliz.ifrs.edu.br>
2025-05-21 16:57:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $a = 10;
-        $b = "10";
-        
-        echo "Teste 1: " .($a==$b);
-        echo "Teste 2: " .($a===$b);
-    ?>
+<?php
+    for($i = 1; $i <= 20; $i++){
+        echo $i. " ";
+        }
+
+?>
+<?php
+    echo " | "; 
+    for($i = -10; $i <= 10; $i++){
+        echo $i. " ";
+        }
+?>
+<?php
+    echo " | "; 
+    for($i = -15; $i <= -5; $i++){
+        echo $i. " ";
+        }
+?>
+<?php
+    echo " | "; 
+    for($i = 100; $i <= 200; $i++){
+        if ($i % 2 == 1){
+             echo $i. " ";
+        }
+        }
+?>
 </body>
 </html>
